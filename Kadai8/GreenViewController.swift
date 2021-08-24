82db9a88cdbf3e86ac3bca2e4f07b7f2a3689a9e
吉田晃崇 <yoshidaakitaka@yoshidaakiratakashinoMacBook-Pro.local>
2021-08-24 12:57:28 +0900

    first commit

---
+++ Kadai8/GreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GreenViewController.swift
+//  Kadai8
+//
+//  Created by 吉田晃崇 on 2021/08/17.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    @IBOutlet private weak var valueLabel: UILabel!
+    @IBOutlet private weak var sliderValue: UISlider!
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sliderValue.value = delegate.sharedValue
+        valueLabel.text = delegate.sharedString
+        
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+        let num2 = sliderValue.value
+        valueLabel.text = String(num2)
+        delegate.sharedValue = sliderValue.value
+        delegate.sharedString = valueLabel.text!
+    }      
+}
---
+++ Kadai8/RedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Kadai8
+//
+//  Created by 吉田晃崇 on 2021/08/16.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+   
+    @IBOutlet private weak var valueLabel: UILabel!
+    @IBOutlet private weak var sliderValue: UISlider!
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sliderValue.value = delegate.sharedValue
+        valueLabel.text = delegate.sharedString
+       
+    }   
+
+    @IBAction func slider(_ sender: Any) {
+        let num1 = sliderValue.value
+        valueLabel.text = String(num1)
+        delegate.sharedValue = sliderValue.value
+        delegate.sharedString = valueLabel.text!
+    }    
+}
+
